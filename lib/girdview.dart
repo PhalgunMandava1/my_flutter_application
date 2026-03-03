ea54c66e564bd0f668c91b9da31eb3142e2aee80
PhalgunMandava1 <phalgunmandava@gmail.com>
2026-03-03 16:01:07 +0530

    Update girdview.dart

--- lib/girdview.dart
+++ lib/girdview.dart
@@ -1,1 +1,148 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StatsPage(),
+    );
+  }
+}
+
+class StatsPage extends StatefulWidget {
+  const StatsPage({super.key});
+
+  @override
+  State<StatsPage> createState() => _StatsPageState();
+}
+
+class _StatsPageState extends State<StatsPage>
+    with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  Widget statCard(String title, String value, Color color) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title,
+              style: const TextStyle(color: Colors.white70)),
+          const SizedBox(height: 10),
+          Text(value,
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)),
+        ],
+      ),
+    );
+  }
+
+  Widget statsGrid() {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 50,
+        mainAxisSpacing: 50,
+        children: [
+          statCard("Heart Rate", "120 bpm", Colors.redAccent),
+          statCard("Sleep", "8h 40m", Colors.deepPurple),
+          statCard("Calories", "600 kcal", Colors.orange),
+          statCard("Steps", "11,115", Colors.blue),
+          statCard("Running", "4.0 km", Colors.indigo),
+          statCard("Cycling", "5.0 km", Colors.green),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF2F3F7),
+
+      appBar: AppBar(
+        title: const Text("STATS"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Colors.blue,
+          unselectedLabelColor: Colors.grey,
+          tabs: const [
+            Tab(text: "Today"),
+            Tab(text: "Weekly"),
+            Tab(text: "Monthly"),
+          ],
+        ),
+      ),
+
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          statsGrid(),
+          const Center(
+            child: Text(
+              "Weekly Data Coming Soon",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+    
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: "Stats",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label:"Rewards",
+          )
+        ],
+      ),
+    );
+  }
+}
