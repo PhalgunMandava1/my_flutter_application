60608894e496ccae74c43ba84d74386c790bfb26
PhalgunMandava1 <phalgunmandava@gmail.com>
2026-03-05 17:35:11 +0530

    Update Google_page.dart

--- lib/Google_page.dart
+++ lib/Google_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class GooglePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Google")),
+      body: Center(
+        child: Text("Google Page", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
