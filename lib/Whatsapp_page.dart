ca99b4939516644ab013286b549cb464bd555f36
PhalgunMandava1 <phalgunmandava@gmail.com>
2026-03-05 17:36:11 +0530

    Update Whatsapp_page.dart

--- lib/Whatsapp_page.dart
+++ lib/Whatsapp_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class WhatsappPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Whatsapp")),
+      body: Center(
+        child: Text("Whatsapp Page", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
