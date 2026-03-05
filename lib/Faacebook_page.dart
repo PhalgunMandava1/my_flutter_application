6d5ecd92b0a3489f4106fdca9c38c565f0e50703
PhalgunMandava1 <phalgunmandava@gmail.com>
2026-03-05 17:34:48 +0530

    Update Facebook_page.dart

--- lib/Faacebook_page.dart
+++ lib/Faacebook_page.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class FacebookPage extends StatelessWidget {
+  const FacebookPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Facebook"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Open Details Page"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const FacebookPage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
