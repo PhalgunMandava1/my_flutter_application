256f1d46f4bb280bd1dc0020e6d80656f7c048c9
PhalgunMandava1 <phalgunmandava@gmail.com>
2026-03-05 16:12:35 +0530

    Update stats_page.dart

--- lib/stats_page.dart
+++ lib/stats_page.dart
@@ -1,160 +1,83 @@
 import 'package:flutter/material.dart';
 
-class StatsPage extends StatelessWidget {
-  const StatsPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "STATS",
-            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Day"),
-              Tab(text: "Week"),
-              Tab(text: "Month"),
-            ],
-          ),
-        ),
-
-        body: TabBarView(
+  void showOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (sheetContext) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            ListTile(
+              leading: const Icon(Icons.share),
+              title: const Text("Share"),
+              onTap: () {
+                Navigator.pop(sheetContext);
 
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 50,
-                mainAxisSpacing: 50,
-                children: [
-
-                  statCard(
-                    "Heart Rate",
-                    "110 bpm",
-                    "Healthy",
-                    Colors.red,
-                    Icons.monitor_heart,
-                  ),
-
-                  statCard(
-                    "Sleep",
-                    "8.5 h",
-                    "Good",
-                    Colors.purple,
-                    Icons.bedroom_parent,
-                  ),
-
-                  statCard(
-                    "Calories",
-                    "789 kcal",
-                    "Burned",
-                    Colors.orange,
-                    Icons.sports_gymnastics,
-                  ),
-
-                  statCard(
-                    "Steps",
-                    "1116",
-                    "Today",
-                    Colors.blueAccent,
-                    Icons.directions_walk_sharp,
-                  ),
-
-                  statCard(
-                    "Worktime",
-                    "8 h",
-                    "Active",
-                    Colors.deepPurple,
-                    Icons.work,
-                  ),
-
-                  statCard(
-                    "Running",
-                    "20 MIN",
-                    "Workout",
-                    Colors.green,
-                    Icons.directions_run,
-                  ),
-                ],
-              ),
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Share button opened")),
+                );
+              },
             ),
 
-            const Center(
-              child: Text(
-                "This is Week Page",
-                style: TextStyle(fontSize: 20),
-              ),
+            ListTile(
+              leading: const Icon(Icons.link),
+              title: const Text("Get link"),
+              onTap: () {
+                Navigator.pop(sheetContext);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Link generated")),
+                );
+              },
             ),
 
-            const Center(
-              child: Text(
-                "This is Month Page",
-                style: TextStyle(fontSize: 20),
-              ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text("Edit name"),
+              onTap: () {
+                Navigator.pop(sheetContext);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Edit name clicked")),
+                );
+
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(Icons.delete),
+              title: const Text("Delete collection"),
+              onTap: () {
+                Navigator.pop(sheetContext);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Collection deleted")),
+                );
+              },
             ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 
-  static Widget statCard(
-    String title,
-    String value,
-    String bottomText,
-    Color color,
-    IconData icon,
-  ) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
       ),
 
-      child: Stack(
-        children: [
-
-          Positioned(
-            top: 12,
-            left: 12,
-            child: Text(
-              title,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ),
-
-          Positioned(
-            top: 10,
-            right: 10,
-            child: Icon(icon, color: Colors.white70, size: 30),
-          ),
-
-          Center(
-            child: Text(
-              value,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          Positioned(
-            bottom: 12,
-            right: 12,
-            child: Text(
-              bottomText,
-              style: const TextStyle(color: Colors.white70, fontSize: 14),
-            ),
-          ),
-        ],
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showOptions(context);
+          },
+          child: const Text("Open Bottom Sheet"),
+        ),
       ),
     );
   }
